d8d6e6c1afc1633811cba1ca2b7db5b5e23cc757
unknown <vengat161193@gmail.com>
2017-07-31 11:28:14 +0530

    modified class

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
     public static void main( String[] args )
     {
-    	for(int i=0;i<=300;i++){
+    	for(int i=0;i<=10;i++){
     	
         System.out.println( "Hai vengat"+i );
     }
